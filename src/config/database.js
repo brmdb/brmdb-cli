cd984f6b903482e7812f313be447d4f9f59df085
Alessandro Jean <alessandrojean@gmail.com>
2019-11-16 21:49:22 -0300

    Fix wrong path of the database.

--- src/config/database.js
+++ src/config/database.js
@@ -2,6 +2,6 @@ const path = require('path')
 
 module.exports = {
   dialect: 'sqlite',
-  storage: path.resolve(__dirname, '../../../data/database.sqlite'),
+  storage: path.resolve(__dirname, '../../../brmdb-data/database.sqlite'),
   logging: false
 }
